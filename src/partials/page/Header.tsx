6f195db97ddeed739eaa3ed0b0658df6b2eda9ee
Martin Piala <pialamar@gmail.com>
2023-04-14 10:32:47 +0100

    add link back to the main page in the menu

--- src/partials/page/Header.tsx
+++ src/partials/page/Header.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
     return (
         <header className="bg-blue-200 sticky left-0 top-0 right-0 h-20 justify-start flex items-center">
             <nav className="border-2 border-red-200 flex flex-row items-center gap-8">
-                <h1 className="font-bold text-lg"> TorchViz </h1>
+                <h1 className="font-bold text-lg"> <Link to='/'> TorchViz </Link> </h1>
                 <span> Menu Item </span>
             </nav>
         </header>
